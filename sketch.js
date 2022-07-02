ecb0325ce85ce6c5d45c75964d05afe6af8a1275
AmanjeetCogoport <108551871+AmanjeetCogoport@users.noreply.github.com>
2022-07-03 00:57:29 +0530

    added setup and draw function to the project which will take care of how this game will work

--- sketch.js
+++ sketch.js
@@ -181,7 +181,7 @@ class Ball {
 
 
 
-  // init takes 1 parameter g; if g is true, the ball moves; if g is false ball stay still.
+  // startGame takes 1 parameter g; if g is true, the ball moves; if g is false ball stay still.
 
 function startGame(g) {
     briks = [];
@@ -227,3 +227,65 @@ function startGame(g) {
     }
     
   }
+
+
+
+
+
+
+// exection of game starts from here
+
+function setup() {
+    createCanvas(800, 600);
+  
+    startGame(false);
+  }
+  
+  // this function run after every 10 milli second
+  function draw() {
+    background(0);
+    show_score();
+  
+    // console.log(briks.length);
+    for (let b = briks.length - 1; b >= 0; b--) {
+      let brik = briks[b];
+      if (
+        ball.pos.x > brik.x &&
+        ball.pos.x < brik.x + brik.l &&
+        ball.pos.y + ball.r > brik.y &&
+        ball.pos.y - ball.r < brik.y + brik.h
+      ) {
+        if (brik.state >= 0) {
+          score += brik.state * 5;
+          ball.v.y *= -1;
+          if (brik.updateState()) {
+            briks.slice(b, 1);
+  
+            
+
+
+              
+            }
+          }
+        }
+      }
+  
+      brik.show();
+    }
+  
+    // Update ball and pad
+    ball.update(pad);
+  
+    // Checking key for controlling pad
+    padControl();
+  
+    // Showing elements
+    pad.show();
+  
+  
+
+    
+    
+    }
+  }
+  
